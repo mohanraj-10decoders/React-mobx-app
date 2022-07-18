ba4dffe462851f50382023d7c2808533712db332
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-18 11:57:50 +0530

    Basic counter functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useContext, useState, createContext } from 'react';
+import { useLocalStore, useObserver } from 'mobx-react';
+
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    counter: 0,
+    change: (value) => {
+      store.counter = value == 0 ? 0 : store.counter + value;
+    },
+  }));
+
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+
+const Counter = () => {
+  const store = useContext(StoreContext);
+  return useObserver(() => {
+    return (
+      <div className='counter'>
+        <h3>{store.counter}</h3>
+      </div>
+    );
+  });
+};
+
+const Buttons = () => {
+  const store = useContext(StoreContext);
+  return (
+    <div className='buttons'>
+      <button onClick={() => store.change(-1)}>Decrease</button>
+      <button onClick={() => store.change(0)}>RESET</button>
+      <button onClick={() => store.change(1)}>Increase</button>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className='App'>
-      <h1>MobX</h1>
-    </div>
+    <StoreProvider>
+      <div className='App'>
+        <h1>MobX</h1>
+        <Counter />
+        <Buttons />
+      </div>
+    </StoreProvider>
   );
 }
 
---
+++ src/Store.js
@@ -0,0 +1,15 @@
+// export function counterstore(){
+//     return(
+//         {
+//             count:0,
+//             setcount(value){
+//                 if(value === '+')
+//                 this.count += 1
+//                 else if (value === '-')
+//                 this.count -= 1
+//                 else
+//                 this.count = 0
+//             }
+//         }
+//     )
+// }
